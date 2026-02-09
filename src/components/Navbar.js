f7e4cdcc37f7e2fed203642512e8b66e2c76285d
Joelfernandes30 <joelfernandes@gmail.com>
2026-02-09 12:31:41 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.mylink}</a>
+                            <Link className="nav-link" to="/">{props.mylink}</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" href="/about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <Link className="nav-link active" to="/about" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 About Us
-                            </a>
+                            </Link>
                             <ul className="dropdown-menu"></ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="mytextutils_project/contact">Contact Us</a>
+                            <Link className="nav-link" to="/contact">Contact Us</Link>
                         </li>
                             <li className="nav-item">
-                            <a className="nav-link" href="/card">Cards</a>
+                            <Link className="nav-link" to="/card">Cards</Link>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" aria-disabled="true">Disabled</a>
